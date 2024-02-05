f2f8851595fe030f310753801adbd74f1aed5af3
Arne Nilsen <arne.nilsen@skatteetaten.no>
2024-02-05 22:04:33 +0100

    Refactoring

--- src/greeting/repository.rs
+++ src/greeting/repository.rs
@@ -8,6 +8,47 @@ use uuid::Uuid;
 
 use crate::greeting::service::{Greeting, GreetingRepository, ServiceError};
 
+#[async_trait]
+impl GreetingRepository for SqliteStudentRepository<Postgres> {
+    async fn all(&self) -> Result<Vec<Greeting>, ServiceError> {
+        let greetings = sqlx::query_as!
+        (GreetingEntity, "SELECT id, \"from\", \"to\", heading, message, created FROM greeting ")
+            .fetch_all(&self.pool).await;
+
+
+        Ok(match greetings {
+            Ok(v) => v.iter().map(|v| Greeting::from(v.clone())).collect::<Vec<_>>(),
+            Err(_)=> return Err(ServiceError::UnrecognizedGreetingError)
+        })
+    }
+
+    async  fn store(&mut self, greeting: Greeting) -> Result<(), ServiceError> {
+        let new_greeting = GreetingEntity::from(greeting);
+        let res = sqlx::query_as!(GreetingEntity,"INSERT INTO greeting(id, \"from\", \"to\", heading, message, created) VALUES ($1, $2, $3, $4, $5, $6)",
+            new_greeting.id, new_greeting.from,new_greeting.to, new_greeting.heading, new_greeting.message, new_greeting.created)
+            .fetch_all(&self.pool).await;
+        // block_on(res).expect("Failed to store new greeting");
+        Ok(())
+    }
+}
+
+
+pub struct SqliteStudentRepository<T: sqlx::Database> {
+    pool: Pool<T>,
+}
+
+impl  SqliteStudentRepository<Postgres> {
+    pub async fn new(database_url: &str) -> Result<Self, RepoError> {
+
+        let pool = PgPoolOptions::new()
+            .max_connections(100)
+            .connect(database_url).await?;
+        migrate!("./migrations")
+            .run(&pool).await?;
+        Ok(SqliteStudentRepository{pool})
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct GreetingEntity {
     pub(crate) id: Uuid,
@@ -79,44 +120,5 @@ impl From<Error> for RepoError{
 impl From<MigrateError> for RepoError {
     fn from(value: MigrateError) -> Self {
         RepoError::DbMigrationError(value)
-    }
-}
-
-pub struct SqliteStudentRepository<T: sqlx::Database> {
-    pool: Pool<T>,
-}
-
-impl  SqliteStudentRepository<Postgres> {
-    pub async fn new(database_url: &str) -> Result<Self, RepoError> {
-
-        let pool = PgPoolOptions::new()
-            .max_connections(100)
-            .connect(database_url).await?;
-        migrate!("./migrations")
-            .run(&pool).await?;
-        Ok(SqliteStudentRepository{pool})
-    }
-}
-#[async_trait]
-impl GreetingRepository for SqliteStudentRepository<Postgres> {
-    async fn all(&self) -> Result<Vec<Greeting>, ServiceError> {
-        let greetings = sqlx::query_as!
-            (GreetingEntity, "SELECT id, \"from\", \"to\", heading, message, created FROM greeting ")
-            .fetch_all(&self.pool).await;
-
-
-         Ok(match greetings {
-             Ok(v) => v.iter().map(|v| Greeting::from(v.clone())).collect::<Vec<_>>(),
-             Err(_)=> return Err(ServiceError::UnrecognizedGreetingError)
-         })
-    }
-
-    async  fn store(&mut self, greeting: Greeting) -> Result<(), ServiceError> {
-         let new_greeting = GreetingEntity::from(greeting);
-        let res = sqlx::query_as!(GreetingEntity,"INSERT INTO greeting(id, \"from\", \"to\", heading, message, created) VALUES ($1, $2, $3, $4, $5, $6)",
-            new_greeting.id, new_greeting.from,new_greeting.to, new_greeting.heading, new_greeting.message, new_greeting.created)
-            .fetch_all(&self.pool).await;
-        // block_on(res).expect("Failed to store new greeting");
-        Ok(())
     }
 }
