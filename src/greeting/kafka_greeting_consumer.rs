81170d21037bc12d07c08886b8f6089de89aa87e
Arne Nilsen <arne.nilsen@skatteetaten.no>
2024-02-12 21:55:25 +0100

    Refactoring kafka consumer with more logging

--- src/greeting/kafka_greeting_consumer.rs
+++ src/greeting/kafka_greeting_consumer.rs
@@ -1,11 +1,8 @@
-use log::info;
+use log::{info, warn};
 use rdkafka::{ClientConfig, ClientContext, Message, TopicPartitionList};
 use rdkafka::config::RDKafkaLogLevel;
-use rdkafka::consumer::{
-    BaseConsumer, Consumer, ConsumerContext, StreamConsumer,
-};
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance, StreamConsumer};
 use rdkafka::error::KafkaResult;
-use rdkafka::message::Headers;
 use serde::{Deserialize, Serialize};
 
 struct CustomContext;
@@ -13,13 +10,13 @@ struct CustomContext;
 impl ClientContext for CustomContext {}
 
 impl ConsumerContext for CustomContext {
-    // fn pre_rebalance(&self, _: &BaseConsumer<Self>, rebalance: &Rebalance) {
-    //     info!("Pre rebalance {:?}", rebalance);
-    // }
-    //
-    // fn post_rebalance(&self, _: &BaseConsumer<Self>, rebalance: &Rebalance) {
-    //     info!("Post rebalance {:?}", rebalance);
-    // }
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        info!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        info!("Post rebalance {:?}", rebalance);
+    }
 
     fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
         info!("Committing offsets: {:?}", result);
@@ -32,39 +29,39 @@ type LoggingConsumer = StreamConsumer<CustomContext>;
 pub(crate) async fn consume_and_print(brokers: &str, group_id: &str, topics: &str) {
     let context = CustomContext;
 
-    let consumer: BaseConsumer = ClientConfig::new()
+    let consumer: LoggingConsumer = ClientConfig::new()
         .set("group.id", group_id)
         .set("bootstrap.servers", brokers)
-        // .set("enable.partition.eof", "false")
-        // .set("session.timeout.ms", "6000")
-        // .set("enable.auto.commit", "true")
-        //.set("statistics.interval.ms", "30000")
-        //.set("auto.offset.reset", "smallest")
+         .set("enable.auto.commit", "false")
         .set_log_level(RDKafkaLogLevel::Debug)
-        // .create_with_context(context)
-        .create()
+        .create_with_context(context)
         .expect("Consumer creation failed");
 
     consumer
         .subscribe(&[&topics])
         .expect("Can't subscribe to specified topics");
 
+    info!(
+        "Listening on topic:{:?} on broker: {:?}",
+        &[&topics],
+        brokers
+    );
     loop {
-        info!("Listening on kafka");
-        for msg_result in consumer.iter() {
-            let msg = msg_result.unwrap();
-
-            let key: &str = msg.key_view().unwrap().unwrap();
-            let value = msg.payload_view::<str>().unwrap().unwrap();
-            let payload: KafkaTopicPayload = serde_json::from_slice(msg.payload().unwrap())
-                .expect("failed to deser JSON to GreetingLogg");
-            info!(
-                "received key {} with value {:?} in offset {:?} from partition {}",
-                key,
-                payload.after,
-                msg.offset(),
-                msg.partition()
-            )
+        match consumer.recv().await {
+            Err(e) => warn!("Kafka error: {}", e),
+            Ok(msg) => {
+                let key: &str = msg.key_view().unwrap().unwrap();
+                let payload: KafkaTopicPayload = serde_json::from_slice(msg.payload().unwrap())
+                    .expect("failed to deser JSON to GreetingLogg");
+                info!(
+                    "received key {} with value {:?} in offset {:?} from partition {}",
+                    key,
+                    payload.after,
+                    msg.offset(),
+                    msg.partition()
+                );
+                consumer.commit_message(&msg, CommitMode::Async).unwrap();
+            }
         }
     }
 }
@@ -75,9 +72,10 @@ pub struct KafkaTopicPayload {
     after: GreetingLogg,
 }
 #[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
+#[serde_with::serde_as]
 pub struct GreetingLogg {
     id: i64,
     greeting_id: String,
-    created: i128,
+    // #[serde_as(as = "TimestampSeconds<i64>")]
+    created: i64,
 }
